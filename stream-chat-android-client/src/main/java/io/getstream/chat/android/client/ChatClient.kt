71a8bc992270342fe3006f812a2ff3c70388a2e5
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-14 16:39:59 -0300

    [CAS-1310] Adding custom api call logger interceptor

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.OnLifecycleEvent
 import io.getstream.chat.android.client.api.ChatApi
 import io.getstream.chat.android.client.api.ChatClientConfig
 import io.getstream.chat.android.client.api.ErrorCall
+import io.getstream.chat.android.client.api.interceptor.HttpLoggingInterceptor
 import io.getstream.chat.android.client.api.models.FilterObject
 import io.getstream.chat.android.client.api.models.QueryChannelRequest
 import io.getstream.chat.android.client.api.models.QueryChannelsRequest
@@ -84,6 +85,7 @@ import io.getstream.chat.android.client.utils.observable.Disposable
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
 import io.getstream.chat.android.core.internal.exhaustive
 import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
 import java.io.File
 import java.nio.charset.StandardCharsets
 import java.util.Calendar
@@ -1560,6 +1562,7 @@ public class ChatClient internal constructor(
             ChatNotificationHandler(appContext)
         private var fileUploader: FileUploader? = null
         private val tokenManager: TokenManager = TokenManagerImpl()
+        private var apiLogInterceptor: Interceptor = HttpLoggingInterceptor()
 
         public fun logLevel(level: ChatLogLevel): Builder {
             logLevel = level
@@ -1593,6 +1596,10 @@ public class ChatClient internal constructor(
 
         public fun disableWarmUp(): Builder = apply {
             warmUp = false
+        }
+
+        public fun apiLoggerInterceptor(interceptor: Interceptor): Builder = apply {
+            apiLogInterceptor = interceptor
         }
 
         public fun baseUrl(value: String): Builder {
@@ -1649,7 +1656,14 @@ public class ChatClient internal constructor(
             )
 
             val module =
-                ChatModule(appContext, config, notificationsHandler, fileUploader, tokenManager, callbackExecutor)
+                ChatModule(appContext,
+                    config,
+                    notificationsHandler,
+                    fileUploader,
+                    tokenManager,
+                    callbackExecutor,
+                    apiLogInterceptor
+                )
 
             val result = ChatClient(
                 config,
